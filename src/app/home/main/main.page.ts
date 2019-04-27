f11636ddc2d76776524d942e75c4eebe1fcf1b41
mansi_aj <mansi.ajgaonkar@onelifecapital.in>
2019-04-27 23:58:31 +0530

    change in new app

--- src/app/home/main/main.page.ts
+++ src/app/home/main/main.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { IonSlides } from '@ionic/angular';
 @Component({
   selector: 'app-main',
   templateUrl: './main.page.html',
@@ -11,5 +11,22 @@ export class MainPage implements OnInit {
 
   ngOnInit() {
   }
+
+  slideOpts= {
+    loop: true, // have to add loop = true, otherwise when reach to last slide if will all the way back to first, not just smooth slide to first
+    };
+    slidesDidLoad(slides:IonSlides) {
+    slides.startAutoplay();
+    }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 600);
+  }
+
 
 }
