d0ffcd4d1aa90b694aae965d0fe7e1c503240e92
Mayur Mhatre <mhatremayur2520@gmail.com>
2019-07-19 18:34:12 +0530

    Samll changes

--- src/app/services/credencials.ts
+++ src/app/services/credencials.ts
@@ -5,5 +5,5 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AppGlobals {
 
-    readonly ApiLink: string = 'http://103.252.7.5:3800';
+    readonly ApiLink: string = 'http://localhost:3800';
 }
